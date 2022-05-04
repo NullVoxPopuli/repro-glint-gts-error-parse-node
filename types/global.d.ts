10fe083833b379a43475003721ffa8ae233913cf
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-05-04 07:39:25 -0400

    ember install ember-cli-typescript

---
+++ types/global.d.ts
@@ -0,0 +1,6 @@
+// Types for compiled templates
+declare module 'repro-glint-gts-error-parse-node/templates/*' {
+  import { TemplateFactory } from 'htmlbars-inline-precompile';
+  const tmpl: TemplateFactory;
+  export default tmpl;
+}
